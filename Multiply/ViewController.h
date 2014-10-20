40af678a150c90565e6474d18db0f06d931ae816
Mobile Making <mm@Alexs-MacBook-Pro-2.local>
2014-10-20 15:26:06 -0700

    Initial Commit

---
+++ Multiply/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by Mobile Making on 10/20/14.
+//  Copyright (c) 2014 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Multiply/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Multiply
+//
+//  Created by Mobile Making on 10/20/14.
+//  Copyright (c) 2014 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
